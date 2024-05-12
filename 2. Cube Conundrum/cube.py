874f308038c69aba33613454b9f48f9444b7f931
Cyrus_Kwan <cyrus.kwan@outlook.com>
2024-05-13 00:36:45 +1000

    Setup for day 2

---
+++ 2. Cube Conundrum/cube.py
@@ -0,0 +1,16 @@
+import re
+
+def current_dir():
+    '''
+    Returns the current directory of __main__
+    '''
+    pattern = r"^.*[/\\]"
+    searches = re.search(pattern, __file__)
+    groups = searches.group(0)
+    return groups
+
+def main():
+    directory = current_dir()
+
+if __name__ == "__main__":
+    main()
---
+++ 2. Cube Conundrum/test_cube.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestCube(unittest):
+    '''
+    Unit tests for cube.py functions
+    '''
+
+if __name__ == "__main__":
+    unittest.main()
